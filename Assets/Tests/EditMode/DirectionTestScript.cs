a3c0cebce969332c9ba668396f07bf6d58e081b5
ДимДим <1000rub@list.ru>
2023-10-26 13:42:54 +0300

    gap 261023

---
+++ Assets/Tests/EditMode/DirectionTestScript.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+
+public class DirectionTestScript
+{
+    [Test]
+    public void TestTest()
+    {
+        Assert.AreEqual(true, true);//вариант для контроля статики (референс,тестируемый)
+    }
+
+}
--- Assets/Tests/temp/NewTestScript.cs
+++ Assets/Tests/temp/NewTestScript.cs
@@ -10,67 +10,46 @@ using UnityEngine.Windows;
 public class NewTestScript
 {
     private Healt _char;
-    //tresh
-    //// A Test behaves as an ordinary method
-    //[Test]
-    //public void NewTestScriptSimplePasses()
-    //{
-    //    // Use the Assert class to test conditions
-    //}
-
-    //// A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    //// `yield return null;` to skip a frame.
 
     [SetUp]//под этим мы указываем метод который определит последовательсность контроля
     public void TestSetup()
     {
-        StartTestMetod();
-    }
-    private void StartTestMetod()
-    {
         SceneManager.LoadScene(0);//загрузим сцену
     }
 
-    [TearDown]
-    public void Teardown()
-    {
-        SceneManager.UnloadScene(0);//загрузим сцену
-        //UnityEngine.Object.Destroy(_char1.gameObject);
-    }
+    //[TearDown]
+    //public void Teardown()
+    //{
+    //    SceneManager.UnloadScene(0);//загрузим сцену
+    //    //UnityEngine.Object.Destroy(_char1.gameObject);
+    //}
 
     [UnityTest]//Первая группа тестов
     public IEnumerator A_PlayerTest()
     {
         yield return new WaitForSeconds(5);
-        FindObjectPlayer();
-        PosTestMetod();
+        _char = GameObject.FindObjectOfType<Healt>();//найдем объект со скриптом
+        UnityEngine.Assertions.Assert.IsNotNull(_char);
+
+        _char.HealtCount = 100;
+        yield return new WaitForSeconds(1);
+        //UnityEngine.Assertions.Assert.IsNotNull(_char);//объект должен быть нулем
 
         //yield return null;
         //Assert.AreEqual(true,true);//проверим что true равен true
     }
 
-    private void FindObjectPlayer()
-    {
-_char = GameObject.FindObjectOfType<Healt>();//найдем объект со скриптом
-
-    }
-
-    private void PosTestMetod()
-    {
-        UnityEngine.Assertions.Assert.IsNotNull(_char);
-    }
-
     [UnityTest]//Вторая группа тестов
     public IEnumerator B_HealtPlayerTest()
     {
-        if (_char == null)
-        {
-            yield return null;
-        }
+        //if (_char == null)
+        //{
+        //    yield return null;
+        //}
 
-        _char.HealtCount = 0;
+        _char.HealtCount = 100;
         yield return new WaitForSeconds(1);
-        UnityEngine.Assertions.Assert.IsNull(_char);//объект должен быть нулем
+        UnityEngine.Assertions.Assert.IsNotNull(_char);//объект должен быть нулем
     }
 
     [UnityTest]//Вторая группа тестов
